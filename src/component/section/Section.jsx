5d9d74e37b5943c83f46f1963f5bfdb757e1c74a
Hemant Khinchi <99024438+HemantKhinchiProjects@users.noreply.github.com>
2024-02-03 04:48:02 +0000

    company section complate

--- src/component/section/Section.jsx
+++ src/component/section/Section.jsx
@@ -1,50 +1,66 @@
-// import "./section.css";
-import { useState } from "react";
 import React from "react";
-// import cardImage from "../assets/Group2252.png";
-import { Companys } from "../../data";
+import { useState } from "react";
+import { companys } from "../../data";
 import { HiCheck } from "react-icons/hi";
+
 export default function Section() {
-  const [details, setdetails] = useState(Companys);
+  function getBackgroundColorClass(title) {
+    const titleHash = title
+      .split("")
+      .reduce((acc, char) => acc + char.charCodeAt(0), 0);
+    const backgroundColors = [
+      "bg-blue-200",
+      "bg-gray-200",
+      "bg-red-200",
+      "bg-purple-50",
+    ];
+    return backgroundColors[titleHash % backgroundColors.length];
+  }
+
   return (
     <main className="bg-slate-50">
       <div className="container">
         <div className="py-4 px-12 mx-4">
           <h1 className="text-3xl font-bold mb-1">Welcome, John Doe</h1>
           <ul className="cards grid grid-cols-3 gap-4">
-            <li className="card-container">
-              <div className="border-4 border-white border-solid rounded-lg shadow p-6 bg-purple-50">
-                <h2 className="flex items-center text-xl font-semibold mb-4">
-                  {details.image}
-                  {details.title}
-                  {/* Utkarsh Small Finance Bank */}
-                </h2>
-                <div className="flex items-center mb-4 justify-between">
-                  <div className="bg-purple-200 flex items-center p-2 rounded-lg text-black">
-                    <HiCheck className="w-4 h-4 mr-1 bg-purple-800 rounded-full text-white" />
-                    Highest Interest Rate
+            {Object.values(companys).map((company, index) => (
+              <li
+                key={index}
+                className={`border-4 border-white border-solid rounded-lg shadow p-6 card-container ${getBackgroundColorClass(company.title)}`}
+              >
+                <div className=" ">
+                  <h2 className="flex items-center text-xl font-semibold mb-4">
+                    {company.title}
+                  </h2>
+                  <div className="flex items-center mb-4 justify-between">
+                    <div className="bg-purple-200 flex items-center p-2 rounded-lg text-black">
+                      <HiCheck className="w-4 h-4 mr-1 bg-purple-800 rounded-full text-white" />
+                      {company.descriptionOne}
+                    </div>
+                    <div className="bg-purple-200 flex items-center p-2 rounded-lg text-black">
+                      <HiCheck className="w-4 h-4 mr-1 bg-purple-800 rounded-full text-white" />
+                      {company.descriptionTne}
+                    </div>
                   </div>
-                  <div className="bg-purple-200 flex items-center p-2 rounded-lg text-black">
-                    <HiCheck className="w-4 h-4 mr-1 bg-purple-800 rounded-full text-white" />
-                    RBI Insured
-                  </div>
-                </div>
-                <div className="flex justify-between justify-center">
-                  <div>
-                    <span>
-                      <p className="text-gray-600 mb-1 text-sm">
-                        Interest Upto
-                      </p>
-                    </span>
-                    <span className="font-bold text-black">9.10% p.a</span>
-                  </div>
+                  <div className="flex justify-between justify-center">
+                    <div>
+                      <span>
+                        <p className="text-gray-600 mb-1 text-sm">
+                          Interest Upto
+                        </p>
+                      </span>
+                      <span className="font-bold text-black">
+                        {company.intreast}% p.a
+                      </span>
+                    </div>
 
-                  <button className="bg-purple-900 text-white rounded-lg px-6 py-2 mt-4">
-                    Book Now
-                  </button>
+                    <button className="bg-purple-900 text-white rounded-lg px-6 py-2 mt-4">
+                      Book Now
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
--- src/data.js
+++ src/data.js
@@ -2,33 +2,33 @@
 // import bajajFcardImage from "../../assets/MaskGroup5.png";
 // import shriramFcardImage from "../../assets/SHRIRAMFINNS-b563e9ad.png";
 // import mahindracardImage from "../../assets/MaskGroup5-1.png";
-export const Companys = [
-  {
+export const companys = {
+  Utkarsh: {
     // image: UtkarshcardImage,
     title: "Utkarsh Small Finance Bank",
     descriptionOne: "Utkarsh Small Finance Bank",
     descriptionTne: "RBI Insured",
     intreast: 9.1,
   },
-  {
+  bajajF: {
     // image: bajajFcardImage,
     title: "Bajaj Finserv",
     descriptionOne: "Crisil AAA Rated",
     descriptionTne: "No Video KYC required",
     intreast: 8.8,
   },
-  {
+  shriramF: {
     // image: shriramFcardImage,
     title: "Shriram Finance",
     descriptionOne: "Crisil AAA Rated",
     descriptionTne: "No Video KYC required",
     intreast: 8.8,
   },
-  {
+  mahindraF: {
     // image: mahindracardImage,
     title: "Mahindra Finance",
     descriptionOne: "Crisil AAA Rated",
     descriptionTne: "No Video KYC required",
     intreast: 8.8,
   },
-];
+};
